6576711630619267ddb28fc66e907eda137722ca
Jarrod Mosen <jarrod.mosen@me.com>
2014-05-04 21:35:40 +1200

    10x faster, no jQuery dependency

--- stripe.js
+++ stripe.js
@@ -1,38 +1,40 @@
-/*
-    STRIPE
+'use strict';
 
-    stripe('body', 1000, 20, 'rgb(20, 30, 40)', ['aaa','bbb','ccc']);
- */
+var iterator,
+    applyStripe = {},
+    target  = document.getElementById('stripe-loader'),
+    canvas  = document.createElement('canvas'),
+    context = canvas.getContext('2d'),
+    width   = 333,
+    height  = 5,
+    time    = 6,
+    colours = ['467F71', 'D3F263', '3E8C84','296B73', 'E52738', 'FEE63F','3BD2CC', '2BA6E8', 'B4D455'],
+    colours_length = colours.length,
+    canvas_width = canvas.width = width,
+    canvas_height = canvas.height = height;
 
-function stripe(element, width, height, colour, palette)
-{
-    "use strict";
+function stripe() {
+  applyStripe = window.setInterval(function () {
+      var cursor = 0,
+          max = (canvas_width / 10),
+          min = 1;
 
-    var iterator,
-        target  = document.querySelector(element),
-        canvas  = document.createElement("canvas"),
-        context = canvas.getContext("2d"),
-        width   = width   || 1000,
-        height  = height  || 5,
-        colour  = colour  || '#666',
-        colours = palette || [
-            '467F71', 'D3F263', '3E8C84',
-            '296B73', 'E52738', 'FEE63F',
-            '3BD2CC', '2BA6E8', 'B4D455'
-        ];
+      while (cursor < canvas_width) {
+        var width = (Math.floor(Math.random() * max) + min);
 
-    canvas.width = width;
-    canvas.height = height;
+        context.fillStyle = '#' + colours[Math.floor(Math.random() * colours_length)];
+        context.fillRect(cursor, 0, width, height);
 
-    for (iterator = 0; iterator < canvas.width; iterator += 1)
-    {
-        // much nicer results with random in here
-        var random = Math.floor(Math.random() * 50);
+        cursor += width;
+      }
 
-        // grab random colour. shove it in.
-        context.fillStyle = "#" + colours[Math.floor(Math.random() * colours.length)];
-        context.fillRect((iterator * random), 0, (50 * random), height);
-    }
-    // let's lather her up
-    target.style.background = colour + " url(" + canvas.toDataURL("image/png") + ") repeat-x";
+      target.style.background = 'url(' + canvas.toDataURL('image/png') + ') repeat-x';
+  }, time);
+
+  target.style.height = '5px';
+}
+
+function stopStripe() {
+    window.clearInterval(applyStripe);
+    target.style.height = 0;
 }
